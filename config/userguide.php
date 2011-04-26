2e13156d5a626606c125ca404ddfef85a942310e
Andrew Hutchings <andrew.hutchings@gmail.com>
2011-04-25 20:14:57 -0400

    Add userguide page. Closes #3

---
+++ config/userguide.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+    'modules' => array(
+        'hoptoad' => array(
+            // Whether this module's userguide pages should be shown
+            'enabled' => TRUE,
+            
+            // The name that should show up on the userguide index page
+            'name' => 'Hoptoad',
+            
+            // A short description of this module, shown on the index page
+            'description' => 'Hoptoad error notifier.',
+            
+            // Copyright message, shown in the footer for this module
+            'copyright' => '&copy; 2011 Andrew Hutchings',
+        ),
+    ),
+);
